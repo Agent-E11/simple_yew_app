f5464cf86b0c5a5d5e6fabf87221fc096269a505
Eamon Burns <eamon.r.burns@gmail.com>
2023-12-25 22:46:14 -0800

    Add css styles, add more buttons

--- dist/yew_app-b000da4476e628a48defa51cc3f882e778d54316dc7e0006e6d778641ac5444a8637138432e5c9a634474df664b929cb.js
+++ dist/yew_app-b000da4476e628a48defa51cc3f882e778d54316dc7e0006e6d778641ac5444a8637138432e5c9a634474df664b929cb.js
@@ -623,12 +623,12 @@ function __wbg_get_imports() {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper840 = function(arg0, arg1, arg2) {
-        const ret = makeClosure(arg0, arg1, 71, __wbg_adapter_20);
+    imports.wbg.__wbindgen_closure_wrapper880 = function(arg0, arg1, arg2) {
+        const ret = makeClosure(arg0, arg1, 86, __wbg_adapter_20);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper3163 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 258, __wbg_adapter_23);
+    imports.wbg.__wbindgen_closure_wrapper3203 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 273, __wbg_adapter_23);
         return addHeapObject(ret);
     };
 
@@ -670,7 +670,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('yew_app-891b31ba2e2cc9522be238008e10f2abe281ee939a6c7935a80abfa970beed76cca3c0f39ba294f82261fe000cdeb903_bg.wasm', import.meta.url);
+        input = new URL('yew_app-b000da4476e628a48defa51cc3f882e778d54316dc7e0006e6d778641ac5444a8637138432e5c9a634474df664b929cb_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
--- src/main.rs
+++ src/main.rs
@@ -4,29 +4,29 @@ use yew::{html, Html, Component, Context};
 
 pub enum Msg {
     Calculate,
-    SetNumber(bool, f32),
-    SetOperator(Operator),
-}
-pub enum Operator {
-    Mul,
-    Div,
-    Add,
-    Sub,
-}
-impl std::fmt::Display for Operator {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::Mul => write!(f, " * "),
-            Self::Div => write!(f, " / "),
-            Self::Add => write!(f, " + "),
-            Self::Sub => write!(f, " - "),
+        SetNumber(bool, f32),
+        SetOperator(Operator),
+    }
+    pub enum Operator {
+        Mul,
+        Div,
+        Add,
+        Sub,
+    }
+    impl std::fmt::Display for Operator {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            match self {
+                Self::Mul => write!(f, " * "),
+                Self::Div => write!(f, " / "),
+                Self::Add => write!(f, " + "),
+                Self::Sub => write!(f, " - "),
+            }
         }
     }
-}
 
-pub struct Calculator {
-    result: f32,
-    number_1: f32,
+    pub struct Calculator {
+        result: f32,
+        number_1: f32,
     number_2: f32,
     operator: Operator,
     history: Vec<f32>,
@@ -42,7 +42,12 @@ impl Component for Calculator {
 
     fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
         match msg {
-            Msg::Calculate => unimplemented!(),
+            Msg::Calculate => match self.operator {
+                Operator::Mul => self.result = self.number_1 * self.number_2,
+                Operator::Div => self.result = self.number_1 / self.number_2,
+                Operator::Add => self.result = self.number_1 + self.number_2,
+                Operator::Sub => self.result = self.number_1 - self.number_2,
+            },
             Msg::SetNumber(set_1, n) => if set_1 { self.number_1 = n } else { self.number_2 = n},
             Msg::SetOperator(o) => self.operator = o,
         }
@@ -54,41 +59,57 @@ impl Component for Calculator {
             <div>
                 <p>{ self.number_1 }{ &self.operator }{ self.number_2 }</p>
                 <div>
-                    <button class="button" onclick={ctx.link().callback(|_| Msg::SetNumber(true, 1.))}>
+                    <button class="small-button" onclick={ctx.link().callback(|_| Msg::SetNumber(true, 1.))}>
                         { "1" }
                     </button>
-                    <button class="button" onclick={ctx.link().callback(|_| Msg::SetNumber(true, 2.))}>
+                    <button class="small-button" onclick={ctx.link().callback(|_| Msg::SetNumber(true, 2.))}>
                         { "2" }
                     </button>
-                    <button class="button" onclick={ctx.link().callback(|_| Msg::SetNumber(true, 3.))}>
+                    <button class="small-button" onclick={ctx.link().callback(|_| Msg::SetNumber(true, 3.))}>
                         { "3" }
                     </button>
+                    <button class="small-button" onclick={ctx.link().callback(|_| Msg::SetOperator(Operator::Mul))}>
+                        { "*" }
+                    </button>
+                    <button class="small-button" onclick={ctx.link().callback(|_| Msg::SetOperator(Operator::Div))}>
+                        { "/" }
+                    </button>
+
                 </div>
                 <div>
-                    <button class="button" onclick={ctx.link().callback(|_| Msg::SetNumber(true, 4.))}>
+                    <button class="small-button" onclick={ctx.link().callback(|_| Msg::SetNumber(true, 4.))}>
                         { "4" }
                     </button>
-                    <button class="button" onclick={ctx.link().callback(|_| Msg::SetNumber(true, 5.))}>
+                    <button class="small-button" onclick={ctx.link().callback(|_| Msg::SetNumber(true, 5.))}>
                         { "5" }
                     </button>
-                    <button class="button" onclick={ctx.link().callback(|_| Msg::SetNumber(true, 6.))}>
+                    <button class="small-button" onclick={ctx.link().callback(|_| Msg::SetNumber(true, 6.))}>
                         { "6" }
+                    </button>
+                    <button class="small-button" onclick={ctx.link().callback(|_| Msg::SetOperator(Operator::Add))}>
+                        { "+" }
+                    </button>
+                    <button class="small-button" onclick={ctx.link().callback(|_| Msg::SetOperator(Operator::Sub))}>
+                        { "-" }
                     </button>
                 </div>
                 <div>
-                    <button class="button" onclick={ctx.link().callback(|_| Msg::SetNumber(true, 7.))}>
+                    <button class="small-button" onclick={ctx.link().callback(|_| Msg::SetNumber(true, 7.))}>
                         { "7" }
                     </button>
-                    <button class="button" onclick={ctx.link().callback(|_| Msg::SetNumber(true, 8.))}>
+                    <button class="small-button" onclick={ctx.link().callback(|_| Msg::SetNumber(true, 8.))}>
                         { "8" }
                     </button>
-                    <button class="button" onclick={ctx.link().callback(|_| Msg::SetNumber(true, 9.))}>
+                    <button class="small-button" onclick={ctx.link().callback(|_| Msg::SetNumber(true, 9.))}>
                         { "9" }
+                    </button>
+                    <button class="wide-button" onclick={ctx.link().callback(|_| Msg::Calculate)}>
+                        { "=" }
                     </button>
                 </div>
 
                 <p class="counter">
-                    {self.result }
+                    { self.result }
                 </p>
             </div>
         }
